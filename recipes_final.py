38b3bb0122bad3a8bcc578f69c6ba94b43a9525e
jbennett979 <jabennet@uvm.edu>
2024-12-02 17:38:57 -0500

    table_schema done

--- recipes_final.py
+++ recipes_final.py
@@ -3,6 +3,41 @@ import sqlite3
 conn = sqlite3.connect("social_media.db")
 cur = conn.cursor()
 
+def table_schema():
+    exit = False
+    while exit == False:
+        print("The table schema will list the table name with all of its columns indented below.")
+        print("Table Schema:")
+        print("recipes-")
+        print("    r_id")
+        print("    r_name")
+        print("    description")
+        print("    rating")
+        print("    time")
+        print("    servings")
+        print("required-")
+        print("    r_id")
+        print("    amount")
+        print("    i_id")
+        print("ingredients-")
+        print("    i_id")
+        print("    i_name")
+        print("    flavor")
+        print("    type")
+        print("")
+        print("a. Show Table Schema Again")
+        print("b. Exit")
+        good_input = False
+        while good_input == False:
+            user = input("=>")
+            match user:
+                case "a":
+                    good_input = True
+                case "b":
+                    exit = True
+                    good_input = True
+                case _:
+                    print("Please enter a or b")
 
 def main():
     print("Welcome to the Recipe Database!!")
@@ -22,12 +57,19 @@ def main():
         user_input = input("=>")
         match user_input:
             case "a":
+                table_schema()
             case "b":
+                print("")
             case "c":
+                print("")
             case "d":
+                print("")
             case "e":
+                print("")
             case "f":
+                print("")
             case "g":
+                print("")
             case "h":
                 print("Logged Out Successfully!")
                 quit = True
